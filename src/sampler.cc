acd15462ba3bb896a83a6bd72e58662372991f68
Sam Xi <samxi@seas.harvard.edu>
2016-06-29 16:56:22 -0400

    Use TCMallocGetenvSafe to get sampling parameter.
    Without this, Sampler::Init() is called before the environment
    variables have been parsed, which results in the sampling code
    never being called.

--- src/sampler.cc
+++ src/sampler.cc
@@ -1,11 +1,11 @@
 // -*- Mode: C++; c-basic-offset: 2; indent-tabs-mode: nil -*-
 // Copyright (c) 2008, Google Inc.
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-// 
+//
 //     * Redistributions of source code must retain the above copyright
 // notice, this list of conditions and the following disclaimer.
 //     * Redistributions in binary form must reproduce the above
@@ -15,7 +15,7 @@
 //     * Neither the name of Google Inc. nor the names of its
 // contributors may be used to endorse or promote products derived from
 // this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -38,6 +38,7 @@
 #include <algorithm>  // For min()
 #include <math.h>
 #include "base/commandlineflags.h"
+#include "getenv_safe.h"
 
 using std::min;
 
@@ -89,6 +90,11 @@ void Sampler::Init(uint32_t seed) {
   for (int i = 0; i < 20; i++) {
     rnd_ = NextRandom(rnd_);
   }
+  // Get env parameters.
+  const char *sample_parameter =
+      TCMallocGetenvSafe("TCMALLOC_SAMPLE_PARAMETER");
+  FLAGS_tcmalloc_sample_parameter =
+      !sample_parameter ? 0 : strtol(sample_parameter, NULL, 10);
   // Initialize counter
   bytes_until_sample_ = PickNextSamplingPoint();
 }
