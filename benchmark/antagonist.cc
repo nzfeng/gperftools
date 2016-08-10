9ed2e9b3608e917e04f7b4077981849f762184b2
Svilen Kanev <skanev@eecs.harvard.edu>
2016-08-09 17:13:55 -0700

    Base antagonist on gauss_free.
    For less trivial cache behavior.
    Also, bump the frequency of trashing.

--- benchmark/antagonist.cc
+++ benchmark/antagonist.cc
@@ -1,34 +1,62 @@
+#include <cassert>
 #include <cstdlib>
 #include <cstdint>
+#include <random>
+#include <vector>
 
 #include "run_benchmark.h"
 
 extern "C" void xiosim_trash_cache() __attribute__ ((noinline));
 void xiosim_trash_cache() { __asm__ __volatile__ ("":::"memory"); }
 
-// Only use five different size classes to represent the best case for the size
-// class cache.
-static void bench_fastpath_throughput(long rep,
-                                      long iterations,
-                                      uintptr_t param)
+size_t sizes[REPEATS][ITERATIONS];
+bool free_outcomes[REPEATS][ITERATIONS];
+void* blocks[REPEATS][ITERATIONS];
+size_t free_ind[REPEATS][ITERATIONS];
+
+static void init_sizes(long rep) {
+    std::random_device rd;
+    std::default_random_engine re(rd());
+
+    std::vector<double> intervals{16, 64, 256, 512};
+    std::vector<double> weights{9, 0, 1};
+    std::piecewise_constant_distribution<> dist(intervals.begin(), intervals.end(), weights.begin());
+
+    for (size_t i = 0; i < ITERATIONS; i++) {
+        sizes[rep][i] = dist(re);
+    }
+
+    std::default_random_engine re2(rd());
+    std::uniform_int_distribution<size_t> d(0, ITERATIONS);
+    free_ind[rep][0] = 0;
+    for (size_t i = 1; i < ITERATIONS; i++) {
+        free_ind[rep][i] = static_cast<size_t>(d(re2)) % i;
+    }
+}
+
+static void bench_fastpath_gauss(long rep, long iterations, uintptr_t param)
 {
-  size_t sz = 8;
-  for (; iterations>0; iterations--) {
-    void *p = malloc(sz);
+  for (long i = 1; i < iterations; i++) {
+    void *p = malloc(sizes[rep][i]);
     if (!p) {
       abort();
     }
-    free(p);
-    // this makes next iteration use different free list. So
-    // subsequent iterations may actually overlap in time.
-    sz = (sz & 127) + 32;
+    blocks[rep][i] = p;
 
+    size_t to_free = free_ind[rep][i];
+    assert(to_free < i);
+    if (blocks[rep][to_free] != nullptr) {
+        free(blocks[rep][to_free]);
+        blocks[rep][to_free] = nullptr;
+    }
     if (iterations % 8 == 0)
         xiosim_trash_cache();
+
   }
 }
 
+
 int main(void) {
-  report_benchmark("bench_fastpath_throughput", bench_fastpath_throughput, nullptr, 0);
+  report_benchmark("bench_antagonist", bench_fastpath_gauss, init_sizes, 0);
   return 0;
 }
